36c9783c4a333e5ce1e90c48ab326473507fe299
ci <ci@cis-macbook-pro.(none)>
2010-08-09 21:54:17 -0700

    This seems to work better

--- pivotal_workstation/recipes/turn_on_postfix.rb
+++ pivotal_workstation/recipes/turn_on_postfix.rb
@@ -3,11 +3,11 @@ execute "unload postfix" do
 end
 
 execute "set postfix to always run" do
-  command "defaults write /System/Library/LaunchDaemons/org.postfix.master.plist RunAtLoad -bool true"
+  command "defaults write /System/Library/LaunchDaemons/org.postfix.master RunAtLoad -bool true"
 end
 
 execute "set postfix to not run on demand" do
-  command "defaults write /System/Library/LaunchDaemons/org.postfix.master.plist OnDemand -bool false"
+  command "defaults write /System/Library/LaunchDaemons/org.postfix.master OnDemand -bool false"
 end
 
 execute "start up postfix" do
