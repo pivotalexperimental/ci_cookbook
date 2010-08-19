1f21fe85a52b128eec90e2ff45619284be8857ce
Matthew Kocher <ci@cis-macbook-pro.(none)>
2010-08-18 22:52:30 -0700

    recipe names changed in wschef

--- pivotal_workstation/recipes/linked_workstation_recipe.rb
+++ pivotal_workstation/recipes/linked_workstation_recipe.rb
@@ -1,9 +1,9 @@
 log "Workstation Cookbook Linked Recipe Activated"
 
-include_recipe "pivotal_workstation::bash_better_history"
+include_recipe "pivotal_workstation::bash_profile-better_history"
 include_recipe "pivotal_workstation::bash_path_order"
 include_recipe "pivotal_workstation::bash_profile"
-include_recipe "pivotal_workstation::bash_profile_no_sudo_gem_install"
+include_recipe "pivotal_workstation::bash_profile-no_sudo_gem_install"
 include_recipe "pivotal_workstation::finder_display_full_path"
 include_recipe "pivotal_workstation::git_config_global_defaults"
 include_recipe "pivotal_workstation::inputrc"
