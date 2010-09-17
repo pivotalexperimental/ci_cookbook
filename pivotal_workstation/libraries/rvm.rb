7bcea34a6d3bbb6c7e228683eaabce417df0eb21
Matthew Kocher <kocher@gmail.com>
2010-09-16 17:28:33 -0700

    oops, hardcoded user

--- pivotal_workstation/libraries/rvm.rb
+++ pivotal_workstation/libraries/rvm.rb
@@ -2,7 +2,7 @@ class Chef::Resource::Script
   def script_with_rvm(rvm_version, command)
     <<-EOH
     export PATH=#{WS_HOME}/.rvm/gems/#{rvm_version}/bin:#{WS_HOME}/.rvm/gems/#{rvm_version}@global/bin:#{WS_HOME}/.rvm/rubies/#{rvm_version}/bin:#{WS_HOME}/.rvm/bin:$PATH
-    sudo -u ci sh -c "#{command}"
+    sudo -u #{WS_USER} sh -c "#{command}"
     EOH
   end
 end
